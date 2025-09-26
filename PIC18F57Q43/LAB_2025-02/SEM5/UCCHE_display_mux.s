0e4f28b72e45e465b3ec876a663fc6826f61ec70
jedg73 <57274038+jedg73@users.noreply.github.com>
2025-09-26 17:28:36 -0500

    Update and rename Hola_display_mux.s to UCCHE_display_mux.s

---
+++ PIC18F57Q43/LAB_2025-02/SEM5/UCCHE_display_mux.s
@@ -0,0 +1,98 @@
+PROCESSOR 18F57Q43
+#include "cabecera.inc"
+
+    PSECT upcino, class=CODE, reloc=2, abs
+
+upcino:
+    ORG 000000H		;vector de reset
+    bra configuro	;salto a etiqueta configuro
+    
+    ORG 000123H
+   
+    espaciox1:	DB 00H, 00H, 00H, 00H
+    doctor:	DB 5EH, 5CH, 58H, 78H, 5CH, 50H
+    espaciox2:	DB 00H
+    ucchelly:	DB 3EH, 58H, 58H, 74H, 7BH, 30H, 30H, 6EH
+    espaciox3:	DB 00H, 00H, 00H
+    ORG 000200H		;zona de programa de usuario
+
+configuro:
+    ;configuracion de la fuente de reloj
+    movlb 0H		;me voy al bank 0
+    movlw 60H		
+    movwf OSCCON1, 1	;OSCCON1 
+    movlw 02H
+    movwf OSCFRQ, 1
+    movlw 40H
+    movwf OSCEN, 1	;OSCEN
+    
+    ;configuracion de las E/S:
+
+    movlb 4H
+    movlw 80H
+    movwf TRISD, 1
+    movwf ANSELD, 1
+    movlw 0F0H
+    movwf TRISB, 1
+    movwf ANSELB, 1
+    ;CONF TBLPTR
+    clrf TBLPTRU, 1
+    movlw 01H
+    movwf TBLPTRH, 1
+    movlw 27H
+    movwf TBLPTRL, 1
+    ;condiciones iniciales
+    clrf LATB,1
+    clrf LATD, 1
+    
+    
+inicio:
+    call multiplexacion
+    bra inicio
+ 
+    
+multiplexacion:
+    movlw 27H
+    movwf TBLPTRL,1
+    TBLRD*
+    movf TABLAT, 0, 1
+    movwf LATD, 1
+    bsf LATB, 0 ,1
+    call miniretardo
+    bcf LATB, 0 ,1
+    incf TBLPTRL
+    TBLRD*
+    movf TABLAT, 0, 1
+    movwf LATD, 1
+    bsf LATB, 1 ,1
+    call miniretardo
+    bcf LATB, 1 ,1
+    incf TBLPTRL
+    TBLRD*
+    movf TABLAT, 0, 1
+    movwf LATD, 1
+    bsf LATB, 2 ,1
+    call miniretardo
+    bcf LATB, 2 ,1
+    incf TBLPTRL
+    TBLRD*
+    movf TABLAT, 0, 1
+    movwf LATD, 1
+    bsf LATB, 3 ,1
+    call miniretardo
+    bcf LATB, 3 ,1
+   
+    return
+    
+miniretardo:
+    movlb 5H
+    movlw 20
+    movwf 500H, 1
+    nop
+    decfsz 500H, 1, 1
+    movlb 4H
+    return
+    
+    
+    
+    end upcino
